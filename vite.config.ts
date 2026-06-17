ce46d757b3ffd607d93ddabb0805409941586f4f
pritud18-jpg <pritud18@gmail.com>
2026-06-16 22:23:40 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/react-refresh'; // Or '@vitejs/plugin-react' depending on your package.json
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
